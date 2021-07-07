e6dc3b61c035c62f0134cbb378d2edab5a5b2b29
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-07 14:43:13 +0900

    usersテーブルの作成

--- db/migrate/20210707033703_devise_create_users.rb
+++ db/migrate/20210707033703_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :email,               null: false, default: ""
+      t.string :encrypted_password,  null: false, default: ""
+      t.string :nickname,            null: false, default: ""
+      t.string :family_name_zenkaku, null: false, default: ""
+      t.string :first_name_zenkaku,  null: false, default: ""
+      t.string :family_name_hurigana,null: false, default: ""
+      t.string :first_name_hurigana, null: false, default: ""
+      t.string :birthday,            null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
