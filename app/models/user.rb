f790f4f9509d3e89d31202df8d95154aff035563
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-10 09:18:38 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         validates :name, presence: true
+         validates :family_name_zenkaku, presence: true
+         validates :first_name_zenkaku, presence: true
+         validates :family_name_hurigana, presence: true
+         validates :first_name_hurigana, presence: true
+         validates :birthday, presence: true
+
 end
