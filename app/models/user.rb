7220c6246ccf6a6f3596817625c3428e3c8f3600
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-13 22:17:23 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :family_name_zenkaku, presence: true
-  validates :first_name_zenkaku, presence: true
-  validates :family_name_hurigana, presence: true
-  validates :first_name_hurigana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name_zenkaku, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+    validates :first_name_zenkaku, format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/}
+    validates :family_name_hurigana, format: { with: /\A([ァ-ン]|ー)+\z/}
+    validates :first_name_hurigana, format: { with: /\A([ァ-ン]|ー)+\z/}
+    validates :birthday
+  end
+
+  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{7,}/}
 end
