ac76caf91883315fa16f8f286fc4c6c7c0b52ac4
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-18 22:00:30 +0900

    バリデーションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :detail_category_id, :detail_status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description 
+    validates :detail_category_id
+    validates :detail_status_id
+    validates :delivery_fee_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+    validates :price
+  end
 end
