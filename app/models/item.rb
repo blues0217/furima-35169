d961da70b2005cbc3fb484d57ec60896f02bc75e
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-22 15:25:57 +0900

    エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,11 +2,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description 
-    validates :detail_category_id
-    validates :detail_status_id
-    validates :delivery_fee_id
-    validates :delivery_area_id
-    validates :delivery_day_id
+    validates :detail_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :detail_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
