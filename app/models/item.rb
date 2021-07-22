7c90078077e1f8ae14e8cda40c9c0fa5c27a6467
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-22 10:11:45 +0900

    出品機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :delivery_area_id
     validates :delivery_day_id
-    validates :price
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end
