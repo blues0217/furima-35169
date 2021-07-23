98aa82b9416cff03db14470611f0565b80c7326b
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-24 08:26:30 +0900

    with_optionsでのまとめ

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,12 +3,14 @@ class Item < ApplicationRecord
   
   with_options presence: true do
     validates :name
-    validates :description 
-    validates :detail_category_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :detail_status_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :delivery_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+    validates :description
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do 
+      validates :detail_category_id
+      validates :detail_status_id
+      validates :delivery_fee_id
+      validates :delivery_area_id
+      validates :delivery_day_id
+    end
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
