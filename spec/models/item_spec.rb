85fad0701e42b9204a6daaa28c1942bc8a58d440
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-24 13:51:21 +0900

    シングルクォートの削除

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,27 +24,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it "カテゴリーが「--」だと登録できない" do
-        @item.detail_category_id = "1"
+        @item.detail_category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Detail category can't be blank")
       end
       it "商品の状態が「--」だと登録できない" do
-        @item.detail_status_id = "1"
+        @item.detail_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Detail status can't be blank")
       end
       it "送料負担が「--」だと登録できない" do
-        @item.delivery_fee_id = "1"
+        @item.delivery_fee_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank")
       end
       it "配送エリアが「--」だと登録できない" do
-        @item.delivery_area_id = "1"
+        @item.delivery_area_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery area can't be blank")
       end
       it "出荷日が「--」だと登録できない" do
-        @item.delivery_day_id = "1"
+        @item.delivery_day_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
@@ -54,17 +54,17 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '販売価格が全角数字だと出品できない' do
-        @item.price = "２０００"
+        @item.price = '２０００'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
       it '販売価格が300未満だと出品できない' do
-        @item.price = "299"
+        @item.price = 299
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
       it '販売価格が10,000,000以上だと出品できない' do
-        @item.price = "10000000"
+        @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
