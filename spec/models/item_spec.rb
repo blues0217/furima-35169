7e871d2a4a64aff4d2bd8e23687a6ecec43f7c60
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-24 13:54:28 +0900

    販売価格のテスト項目追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -68,6 +68,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
+      it '販売価格が半角英数混合では出品できない' do
+        @item.price = '12test'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '販売価格が半角英語だけでは出品できない' do
+        @item.price = 'test'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it 'userが紐付いていないと保存できない' do
         @item.user = nil
         @item.valid?
