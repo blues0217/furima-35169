5f6b29a2214219abeaeab3dff566d21508f88712
blues0217 <s6.inoue@littlestep.co.jp>
2021-07-31 18:17:40 +0900

    items_controller.rbの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :security_tem, except: [:index, :new, :create, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @item = Item.order(created_at: :desc)
   end
@@ -18,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to new_user_session_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -38,5 +37,13 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :description, :detail_category_id, :detail_status_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def security_tem
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
