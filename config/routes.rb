0339819278783c1017d191da00406b63f937ef2d
blues0217 <s6.inoue@littlestep.co.jp>
2021-08-07 12:19:03 +0900

    indexの編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'items/index'
   devise_for :users
   root to:"items#index"
-  resources :items
-  resources :orders, only:[:index, :create]
+  resources :items do
+    resources :orders, only:[:index, :create]
+  end
 end
