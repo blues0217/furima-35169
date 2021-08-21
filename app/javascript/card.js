f6f1e997894bba200851a88355233c22aa70b46f
blues0217 <s6.inoue@littlestep.co.jp>
2021-08-21 18:55:32 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_24b31f45d0e5b4b13a6576cd");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
