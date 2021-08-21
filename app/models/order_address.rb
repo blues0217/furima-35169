10f3a0f4d0823f1f41efb249fb563f637275d3f4
blues0217 <s6.inoue@littlestep.co.jp>
2021-08-21 18:16:04 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,6 +7,8 @@ class OrderAddress
     validates :item_id
     validates :phone_number
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :house_number
     validates :token
   end
   validates :delivery_area_id, numericality: {other_than: 1, message: "can't be blank"}
